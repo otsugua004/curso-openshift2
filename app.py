e943bf997de98aaf2eae3530b5b0afc86bc1f085
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-10-24 18:25:47 -0500

    Llamar a servicio 1 desde este servicio

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, request
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
-@app.route('/saludo', methods=['GET'])
-def saludo():
-    nombre = request.args.get('nombre', 'Mundo')
-    return f'Hola mundo, {nombre}!', 200
+@app.route('/saludo/<nombre>')
+def llamar_servicio(nombre):
+    url_objetivo = f'http://otsugua-otsugua04-dev.apps.rm2.thpm.p1.openshiftapps.com/saludo?nombre={nombre}'  # Reemplaza con tu API real
+    try:
+        respuesta = requests.get(url_objetivo)
+        datos = respuesta.json()
+        return jsonify(datos)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
-    
