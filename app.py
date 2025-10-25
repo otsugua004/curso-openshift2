0f2c909a312fa15b910972694576428b13256e18
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-10-24 21:50:00 -0500

    fix: solucionar servicio

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 import requests
 
 app = Flask(__name__)
 
-@app.route('/saludo', methods=['GET'])
+@app.route('/saludo')
 def llamar_servicio():
-    nombre = request.args.get('nombre', 'Mundo')
+    nombre = request.args.get('nombre', 'Invitado')  # Valor por defecto si no se pasa el parámetro
     url_objetivo = f'http://otsugua-otsugua04-dev.apps.rm2.thpm.p1.openshiftapps.com/saludo?nombre={nombre}'  # Reemplaza con tu API real
+
     try:
         respuesta = requests.get(url_objetivo)
         datos = respuesta.json()
-        return f'[Servicio 2] Respuesta del servicio 1: , {datos}!'
+        return jsonify(datos)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
+
