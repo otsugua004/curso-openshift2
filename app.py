5b50494305afa4613c0d4cd1eaa57a95cbafd613
otsugua04 <augusto.gonzalez@fisagrp.com>
2025-10-27 17:55:26 -0500

    Incluir comportamiento re inicio de pods

--- app.py
+++ app.py
@@ -14,6 +14,17 @@ def llamar_servicio():
     except Exception as e:
         return f"Error al llamar al servicio 1: {str(e)}", 500
 
+@app.route('/startup', methods=['GET'])
+def startup():
+    return 200
+
+@app.route('/readiness', methods=['GET'])
+def readiness():
+    return 200
+
+@app.route('/health', methods=['GET'])
+def health():
+    return 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
